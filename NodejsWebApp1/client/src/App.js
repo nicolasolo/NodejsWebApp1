0533d0f74a62909c1191b1a9149ce05c56e70d2d
Nicola Solo <solo.nicola@live.com>
2019-03-19 00:49:30 +0200

    aaaa

--- NodejsWebApp1/client/src/App.js
+++ NodejsWebApp1/client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import './App.css';
+import AppNavbar from './components/AppNav';
 
 class App extends Component {
   render() {
     return (
         <div className="App">
+            <AppNavbar />
             <h1>Hello</h1>
       </div>
     );
